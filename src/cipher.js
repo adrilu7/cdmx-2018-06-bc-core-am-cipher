e7840673b04a9b4d858ffb1fd847024be6210913
luz <adrivioleta@hotmail.com>
2018-06-18 01:41:50 -0500

    Cambios css y codigo

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,54 @@
 window.cipher = {
-  // ...
+  //////funciones////////////////////
+  encode:(offset,string) => {// tex llave ya llame con evento
+    let resultado =" ";//  es para que concatene toda la frase y se muestre en DOM
+
+    for (i=0; i<string.length; i++){// inicializa en 0 el recorrido de la frase
+      let cadenas = string.charCodeAt(i);//convierte las letras de la frase en posicion ascii
+
+    if(cadenas >=65 && cadenas <= 90){// para mayusculas ascii
+      resultado += String.fromCharCode((cadenas -65 + offset) %26 + 65);// se convierte las posiciones en letras
+
+      console.log(resultado);
+
+    }else if(cadenas >=97 && cadenas <=122){// para minusculas
+      resultado+= String.fromCharCode((cadenas -97 + offset) %26 + 97 );
+
+
+    }else if (cadenas >=32 && cadenas <=64){// PARA CARACTERES ESPECIALES
+       resultado+=String.fromCharCode(cadenas);
+
+        }
+      }
+     return ("<p>Tu texto cifrado es:</p> "+resultado);
+
+   },
+
+  decode:(offset,string) =>{
+    console.log(string);
+    let resultado =" ";//  es para que concatene toda la frase y se muestre en DOM
+
+    for (i=0; i<string.length; i++){// inicializa en 0 el recorrido de la frase
+      let cadenas = string.charCodeAt(i);//convierte las letras de la frase en posicion ascii
+      //console.log(cadenas);
+
+    if(cadenas >=65 && cadenas <= 90){// para mayusculas ascii
+      resultado += String.fromCharCode((cadenas -90 - offset) %26 + 90);// se convierte las posiciones en letras
+      console.log(resultado);
+
+    }else if(cadenas >=97 && cadenas <=122){// para minusculas
+      resultado+= String.fromCharCode((cadenas -122 - offset) %26 + 122 );
+      console.log(resultado);
+
+    }else if (cadenas >=32 && cadenas <=64){// PARA CARACTERES ESPECIALES
+      resultado+=String.fromCharCode(cadenas);
+
+       }
+        }
+
+        return ("<p>Tu texto descifrado es:</p> "+ resultado);
+      },
+
+      createCipherWithOffset: () =>{}
+
 };
--- src/index.js
+++ src/index.js
@@ -1,101 +1,53 @@
-cifrar = document.getElementById('cifrar');
-descifrar = document.getElementById('descifrar');
+let descifrar = document.getElementById('descifrar');//boton
+let cifrar = document.getElementById('cifrar'); //boton
+let string1 = document.getElementById('entrada'); //estos son input
+let offset1 = document.getElementById('offset');//input
 
-cifrar.addEventListener('click', function(){
-  let texto = document.getElementById('entrada').value; //estos son input
-  let llave = document.getElementById('llave').value;//input
+cifrar.addEventListener('click', evento => {
+ let string= string1.value;
+ let offset =offset1.value;
+ let resul = document.getElementById("resultado");
 
-  if (texto=="" ||  llave=="") { //para ver q no esten vacios
-    console.log("Debes llenar los campos");
-  }
-  else { // si no estan vacios
-    if (/^([0-9])*$/.test(llave)){//verificar q la llave sea num .tex evalua expresion regular
-        if (/^[a-zA-Z]+(\s*[a-zA-Z]*)*[a-zA-Z]+$/.test(texto)) { //si es num verifica q sean min y mayus
-            cifrarTexto(texto,llave);// manda llamar funcion que cifra texto
-        }
-        else {
-          console.log("El texto deben ser solo letras y espacios");
-      }
-    }else {
-      console.log("LLave debe ser un numero");
-    }
-  }
-}, false);
-
-descifrar.addEventListener('click', function(){
-  console.log("HOLA DESCIFRAR");
-  let texto = document.getElementById('entrada').value;
-  let llave = document.getElementById('llave').value;
-  if (texto=="" ||  llave=="") {
-    console.log("Debes llenar los campos");
-  }
-  else {
-    if (/^([0-9])*$/.test(llave)){ //delimita como cambiarlo
-
-      if (/^[a-zA-Z]+(\s*[a-zA-Z]*)*[a-zA-Z]+$/.test(texto)) {
-        descifrarTexto(texto,llave);
-      } else {
-        console.log("El texto deben ser solo letras y espacios");
-      }
-    }else {
-      console.log("LLave debe ser un numero");
-    }
-  }
-}, false);
-
-//////funciones////////////////////
-function cifrarTexto(texto,llave){// tex llave ya llame con evento
-  llave=parseInt(llave)
-  let resultado =" ";//  es para que concatene toda la frase y se muestre en DOM
-
-  for (i=0; i<texto.length; i++){// inicializa en 0 el recorrido de la frase
-    let cadenas = texto.charCodeAt(i);//convierte las letras de la frase en posicion ascii
-
-
-  if(cadenas >=65 && cadenas <= 90){// para mayusculas ascii
-    resultado += String.fromCharCode((cadenas -65 + llave) %26 + 65);// se convierte las posiciones en letras
-    document.getElementById("resultado").innerHTML= "Tu frase cifrada es "+resultado;
-    //console.log((cadenas -65 + (int)llave) %26 + 65);
-    console.log(resultado);
-
-  }else if(cadenas >=97 && cadenas <=122){// para minusculas
-    resultado+= String.fromCharCode((cadenas -97 + llave) %26 + 97 );
-    document.getElementById("resultado").innerHTML= "Tu frase cifrada es " +resultado;
-
-
-  }else if (cadenas >=32 && cadenas <=64){// PARA CARACTERES ESPECIALES
-     resultado+=String.fromCharCode(cadenas);
-     document.getElementById("resultado").innerHTML="Tu frase cifrada es " + resultado;
-      }
-    }
-
+ if (offset== "" ||  string== "")  {  //para ver q no esten vacios
+   alert("Debes llenar los campos");
+ } else { // si no estan vacios
+   if (/^([0-9])*$/.test(offset)){//verificar q la llave sea num .tex evalua expresion regular
+       if (/^[a-zA-Z]+(\s*[a-zA-Z]*)*[a-zA-Z]+$/.test(string)) { //verificar una expresion regular q sean min y mayus
+         document.getElementById("resultado").innerHTML= cipher.encode(parseInt(offset),string);
+           // manda llamar funcion que cifra texto
+       }else {
+         alert("El texto deben ser solo letras y espacios");
+       }
+   }else {
+     alert("LLave debe ser un numero");
+   }
 }
-///////funcion desifrar
-function descifrarTexto(texto,llave){
-  console.log(texto);
-  llave=parseInt(llave)
-  let resultado =" ";//  es para que concatene toda la frase y se muestre en DOM
+});
 
-  for (i=0; i<texto.length; i++){// inicializa en 0 el recorrido de la frase
-    let cadenas = texto.charCodeAt(i);//convierte las letras de la frase en posicion ascii
-    //console.log(cadenas);
+descifrar.addEventListener('click', evento => {
+ let string= string1.value;
+ let offset =offset1.value;
+ let resul = document.getElementById("resultado");
 
-  if(cadenas >=65 && cadenas <= 90){// para mayusculas ascii
-    resultado += String.fromCharCode((cadenas -90 - llave) %26 + 90);// se convierte las posiciones en letras
-    document.getElementById("resultado").innerHTML= "Tu frase decifrada es "+resultado;
-    console.log(resultado);
+ console.log("HOLA DESCIFRAR");
 
-  }else if(cadenas >=97 && cadenas <=122){// para minusculas
-    resultado+= String.fromCharCode((cadenas -122 - llave) %26 + 122 );
-    document.getElementById("resultado").innerHTML= "Tu frase decifrada es " +resultado;
-    console.log(resultado);
+ if (offset=="" ||  string=="") {
+   alert("Debes llenar los campos");
+ }
+ else {
+   if (/^([0-9])*$/.test(offset)){ //delimita como cambiarlo
 
-  }else if (cadenas >=32 && cadenas <=64){// PARA CARACTERES ESPECIALES
-    resultado+=String.fromCharCode(cadenas);
-    document.getElementById("resultado").innerHTML="Tu frase cifrada es " + resultado;
+     if (/^[a-zA-Z]+(\s*[a-zA-Z]*)*[a-zA-Z]+$/.test(string)) {
+     document.getElementById("resultado").innerHTML= cipher.decode(parseInt(offset),string);
+     } else {
+       alert("El texto deben ser solo letras y espacios");
      }
-      }
-    }
+   }else {
+     alert("LLave debe ser un numero");
+   }
+ }
+}, false);
 
 
-    ////<div id="root"></div>
+
+   ////<div id="root"></div>
